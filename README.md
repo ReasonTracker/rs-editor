80e312a37f87c61eb907b8b95026e0e521fe79f8
Bentley Davis <Bentley@BentleyDavis.com>
2023-09-11 21:21:11 -0500

    move readme to about

--- README.md
+++ README.md
@@ -1,22 +1,2 @@
-## To Do
-- routes have different data
-- create a new debate
-- save data to a local file
-- load data from a local file
-- Save data t the cloud
-- Load data from the cloud
-- separate data into calculable, historical, and real(?)
-- add video scripting
-- deletions only delete the edge so far not the child scores, clames or edges. Probably ned to be part of the RSData code.
-
-## Large Packages Used
-
-- [NEXT.js](https://nextjs.org/)
-- [React Flow](https://reactflow.dev/)
-- [React Firebase Hooks - Cloud Firestore](https://github.com/CSFrequency/react-firebase-hooks/tree/master/firestore#react-firebase-hooks---cloud-firestore)
-
-## Deploy
-deployed to https://rseditor.netlify.app/
-
-[![Netlify Status](https://api.netlify.com/api/v1/badges/8ae2a6a9-ba8f-461d-bf22-9dd74f760f0e/deploy-status)](https://app.netlify.com/sites/rseditor/deploys)
+see src\app\about\page.tsx
 
---
+++ src/app/about/page.tsx
@@ -0,0 +1,30 @@
+export default function Page() {
+    return <article >
+        <h2>To Do&apos;s</h2>
+        <ul >
+            <li>routes have different data</li>
+            <li>create a new debate</li>
+            <li>save data to a local file</li>
+            <li>load data from a local file</li>
+            <li>Save data t the cloud</li>
+            <li>Load data from the cloud</li>
+            <li>separate data into calculable, historical, and real(?)</li>
+            <li>add video scripting</li>
+            <li>deletions only delete the edge so far not the child scores, clames or edges. Probably ned to be part of the RSData code.</li>
+        </ul>
+        <h2>Large Packages Used</h2>
+        <ul >
+            <li>https://blueprintjs.com ????</li>
+            <li><a href="https://nextjs.org/" rel="nofollow">NEXT.js</a></li>
+            <li><a href="https://reactflow.dev/" rel="nofollow">React Flow</a></li>
+            <li><a href="https://github.com/CSFrequency/react-firebase-hooks/tree/master/firestore#react-firebase-hooks---cloud-firestore">React Firebase Hooks - Cloud Firestore</a></li>
+        </ul>
+        <h2 >Deploy</h2>
+        <p >deployed to <a href="https://rseditor.netlify.app/" rel="nofollow">https://rseditor.netlify.app/</a></p>
+        <p >
+            <a href="https://app.netlify.com/sites/rseditor/deploys" rel="nofollow">
+                <img src="https://camo.githubusercontent.com/ba7423bea97c2ddd5a1e27e828010f8abc8f57ef626c3aaf480840fce41c4625/68747470733a2f2f6170692e6e65746c6966792e636f6d2f6170692f76312f6261646765732f38616532613661392d626138662d343631642d626632322d3964643734663736306630652f6465706c6f792d737461747573" alt="Netlify Status" data-canonical-src="https://api.netlify.com/api/v1/badges/8ae2a6a9-ba8f-461d-bf22-9dd74f760f0e/deploy-status" style={{ maxWidth: '100%;' }} />
+            </a>
+        </p>
+    </article>
+}
