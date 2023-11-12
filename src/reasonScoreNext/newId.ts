f640a947db855d696503181d5fb9b6321db175f0
Bentley Davis <Bentley@BentleyDavis.com>
2023-11-12 17:10:45 -0600

    NewId increment submillisecond creates

--- src/reasonScoreNext/newId.ts
+++ src/reasonScoreNext/newId.ts
@@ -1,17 +1,35 @@
+import { toBase62 } from "./toBase62";
 
-export function newId(when: Date = new Date()): string {
-  // take the UTC date and convert to base 62
-  let decimal = 5000000000000 - when.getTime();
-  const s = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-  let result = '';
-  while (decimal >= 1) {
-    result = s[(decimal - (62 * Math.floor(decimal / 62)))] + result;
-    decimal = Math.floor(decimal / 62);
-  }
+/**
+ * Generate a short sequential unique-ish id
+ * @param when for testing - The date to use for the id. Defaults to now
+ */
+export const newId = (() => {
+    let lastNum: number;
+    let suffixNum = 0;
 
-  // Add 5 extra random characters in case multiple ids are creates at the same time
-  result += Array(5).join().split(',').map(() => s[(Math.floor(Math.random() * s.length))])
-    .join('');
+    return function (when: Date = new Date()): string {
+        const num = 5000000000000 - when.getTime();
+        let result = toBase62(num);
 
-  return result ;
-}
+        if (num === lastNum) {
+            console.log(`Warning: newId called twice in the same millisecond. ${num} ${lastNum}`);
+            suffixNum++
+        } else {
+            // Generate a random 30-bit number between 2^29 and 2^30 - 1 
+            // which when converted to base 62 will always be 5 characters long
+            // But leaves half the space for incrementing the suffix if the timestamp is the same
+            suffixNum = Math.floor(Math.random() * ((1073741824 + 536870912) / 2 - 536870912) + 536870912);
+        }
+
+        // // Add 5 extra random characters in case multiple ids are creates at the same time
+        result = result + toBase62(suffixNum);
+
+        lastNum = num;
+
+        return result;
+    }
+})();
+
+
+
