79732c4dd3828ad64c803e9bdd9dc7528883eb66
Bentley Davis <bentley@bentleydavis.com>
2024-12-01 09:14:24 -0600

    tweak home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,13 @@ export default function Home() {
             <h1 className="">Reason Tracker</h1>
             <h2>COMING SOON</h2>
 
-            <h2>Decision analysis for all*!</h2>
+            <h2>Decision analysis for all<sup style={{opacity:.5}}>*</sup>!</h2>
             <h3>Videos about hard decisions</h3>
             <p>
                 A video series where you help map out all the reasons for and against controvertial decisions.
             </p>
             <p>
-                Each video analyzes and maps a specific controversial decision and invites audience feedback.
+                Each video will analyze and map a controversial decision and invite audience feedback.
                 New videos will be created based on audience feedback, refining the analysis with each iteration.
                 We hope this will make collective decision-making clearer and more accessible.
 
