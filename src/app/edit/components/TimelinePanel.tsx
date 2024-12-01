9e68d2c1307098d6416e0fc762b4b1bf99d741aa
Bentley Davis <bentley@bentleydavis.com>
2024-12-01 14:45:47 -0600

    demo timeline is working

--- src/app/edit/components/TimelinePanel.tsx
+++ src/app/edit/components/TimelinePanel.tsx
@@ -1,15 +1,67 @@
-import React, { useContext, useState } from "react";
+import React, { use, useContext, useRef, useState } from "react";
 import { FlowDataContext } from "./FlowDataProvider";
-import { Drawer, Button } from "@blueprintjs/core";
+import { Drawer, Button, Slider } from "@blueprintjs/core";
 import { useReactFlow } from "reactflow";
 import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 // import { TextPlugin } from "gsap/TextPlugin";
 // gsap.registerPlugin(TextPlugin)
+
+const buttonStyle = {
+    width: "min-content",
+    margin: "10px",
+    display: "inline-block",
+}
 
 const TimelinePanel = () => {
     const flowDataState = useContext(FlowDataContext);
     const [isPanelOpen, setIsPanelOpen] = useState(false);
     const reactFlowInstance = useReactFlow()
+    const [sliderValue, setsliderValue] = useState(0)
+    const timelineRef = useRef<any>(null);
+
+    useGSAP(() => {
+        let tl = gsap.timeline({
+            paused: true,
+            onUpdate: () => {
+                setsliderValue(tl.progress());
+            }
+        })
+        timelineRef.current = tl;
+        tl.to({text:""}, {
+            text: "This is the animated text!",
+            duration: 3,
+            onUpdate: onUpdate
+        });
+
+        // tl.progress(sliderValue);
+    }, { dependencies: [] });
+
+    function onUpdate(this: any) {
+        const target = this.targets()[0];
+        target.text = this.vars.text.substring(0, this.progress() * this.vars.text.length);
+        // myObject.text = targetText.substring(0, length)
+        flowDataState.dispatch([{
+            type: "modify", newData: {
+                id: flowDataState.debate.mainClaimId || "",
+                type: "claim",
+                content: target.text,
+            }
+        }]);
+
+    }
+
+    const playTimeline = () => {
+        timelineRef.current.play();
+    };
+
+    const pauseTimeline = () => {
+        timelineRef.current.pause();
+    };
+
+    const resetTimeline = () => {
+        timelineRef.current.restart();
+    };
 
     return (
         <div>
@@ -27,11 +79,10 @@ const TimelinePanel = () => {
                 hasBackdrop={false}
                 isOpen={isPanelOpen}
                 onClose={() => setIsPanelOpen(false)}
-                title="Files"
                 canOutsideClickClose={false}
                 canEscapeKeyClose={true}
                 position="bottom"
-                size={"100px"}
+                size={"200px"}
                 className={"bp5-dark"}
                 usePortal={false}
                 enforceFocus={false}
@@ -40,60 +91,30 @@ const TimelinePanel = () => {
                     { position: "fixed" } // Odd that this is needed Otherwise the drawer is transparent
                 }
             >
+                <div>
+                    <Button
+                        onClick={() => {
 
-                <Button
-                    onClick={() => {
-                        const dummyObject = { text: "" };
-                        var tl = gsap.timeline({});
-                        tl.to(dummyObject, {
-                            text: "This is the animated text!",
-                            duration: 3,
-                            onUpdate: onUpdate
-                            // flowDataState.dispatch([{
-                            //     type: "modify", newData: {
-                            //         id: flowDataState.debate.mainClaimId || "",
-                            //         type: "claim",
-                            //         content: dummyObject.text,
-                            //     }
-                            // }]);
-                            // @ts-ignore
-                            //     console.log(this);
-                            // },
-                            // modifiers: {
-                            //     text: (value) => {
-                            //         console.log("value:",value);
-                            //     // Ensure progressive updates for plain object
-                            //     const target = "This is the animated text!";
-                            //     const length = Math.min(value.length, target.length);
-                            //     return target.substring(0, length);
-                            //   },
-                            // },
-                        });
 
-                        function onUpdate(this: any) {
-                            const target = this.targets()[0];
-                            target.text = this.vars.text.substring(0, this.progress() * this.vars.text.length);
-                            // myObject.text = targetText.substring(0, length)
-                            console.log(target.text);
-                            flowDataState.dispatch([{
-                                type: "modify", newData: {
-                                    id: flowDataState.debate.mainClaimId || "",
-                                    type: "claim",
-                                    content: target.text,
-                                }
-                            }]);
-
-                        }
-
-                    }}
-                    style={{
-                        width: "min-content",
-                        marginLeft: "50px",
-                        marginTop: "10px",
-                    }}
-                >
-                    Test
-                </Button>
+                        }}
+                        style={buttonStyle}
+                    >
+                        Test
+                    </Button>
+                    <Button style={buttonStyle} onClick={playTimeline}>Play</Button>
+                    <Button style={buttonStyle} onClick={pauseTimeline}>Pause</Button>
+                    <Button style={buttonStyle} onClick={resetTimeline}>Reset</Button>
+                </div>
+                <div style={{padding:'0 30px'}}>
+                <Slider
+                    min={0}
+                    max={1}
+                    stepSize={0.01}
+                    onRelease={(value: number) => { timelineRef.current.progress(value) }}
+                    value={sliderValue}
+                // vertical={false}
+                    />
+                </div>
 
             </Drawer>
         </div>
@@ -101,3 +122,5 @@ const TimelinePanel = () => {
 };
 
 export default TimelinePanel;
+
+
