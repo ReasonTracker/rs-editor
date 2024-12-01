7fe4487017c9e2d26fda4e78192789545d7e7499
Bentley Davis <bentley@bentleydavis.com>
2024-11-30 19:41:58 -0600

    correct old name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Reason Score Editor',
+  title: 'Reason Tracker Editor',
   description: '',
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Reason Score Editor</h1>
+      <h1>Reason Tracker Editor</h1>
       <Link href="/edit">Editor</Link>
     </main>
   )
